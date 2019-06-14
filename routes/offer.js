99cc0cae9ac262ff75d097495243c9f1e95bb850
Chance Hudson <jchancehud@gmail.com>
2019-06-14 08:10:29 -0500

    Add update offer endpoint

--- routes/offer.js
+++ routes/offer.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose')
 const asyncExpress = require('async-express')
 const auth = require('../middleware/auth')
+const Offer = mongoose.model('Offer')
 
 module.exports = (app) => {
   app.post('/offers', auth, createOffer)
+  app.put('/offers', auth, updateOffer)
   app.get('/offers', auth, loadOffer)
 }
 
 const createOffer = asyncExpress(async (req, res) => {
-  res.json({})
+  const offer = await Offer.create({
+    ...req.body,
+    ownerId: req.user._id,
+  })
+  res.json(offer._doc)
+})
+
+const updateOffer = asyncExpress(async (req, res) => {
+  const { modifiedCount } = await Offer.updateOne({
+    _id: mongoose.types.ObjectId(req.query._id),
+  }, req.body)
+  if (modifiedCount !== 1) {
+    res.status(422).json({ message: 'Modified count is not one' })
+    return
+  }
+  res.status(204).end()
 })
 
 const loadOffer = asyncExpress(async (req, res) => {
-  res.json({})
+  const offer = await Offer.findOne(req.query._id).lean().exec()
+  res.json(offer)
 })
